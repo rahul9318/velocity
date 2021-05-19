02afecc360cbc5046d5e2ca7ca43d9fc5cfe22b8
rahul9318 <rahul12_bhardwaj1819@manavrachna.net>
2021-05-19 20:22:43 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -26,17 +26,17 @@
 					message: "Please enter a message"
 				},
 				/* submit via ajax */
-				
-				submitHandler: function(form) {		
+
+				submitHandler: function(form) {
 					var $submit = $('.submitting'),
 						waitText = 'Submitting...';
 
-					$.ajax({   	
+					$.ajax({
 				      type: "POST",
 				      url: "php/sendEmail.php",
 				      data: $(form).serialize(),
 
-				      beforeSend: function() { 
+				      beforeSend: function() {
 				      	$submit.css('display', 'block').text(waitText);
 				      },
 				      success: function(msg) {
@@ -46,15 +46,15 @@
 		               		$('#contactForm').fadeIn();
 		               	}, 1000);
 				            setTimeout(function(){
-				               $('#form-message-success').fadeIn();   
+				               $('#form-message-success').fadeIn();
 		               	}, 1400);
 
 		               	setTimeout(function(){
-				               $('#form-message-success').fadeOut();   
+				               $('#form-message-success').fadeOut();
 		               	}, 8000);
 
 		               	setTimeout(function(){
-				               $submit.css('display', 'none').text(waitText);  
+				               $submit.css('display', 'none').text(waitText);
 		               	}, 1400);
 
 		               	setTimeout(function(){
@@ -62,7 +62,7 @@
 											    this.reset();
 											});
 		               	}, 1400);
-			               
+
 			            } else {
 			               $('#form-message-warning').html(msg);
 				            $('#form-message-warning').fadeIn();
@@ -74,7 +74,7 @@
 				         $('#form-message-warning').fadeIn();
 				         $submit.css('display', 'none');
 				      }
-			      });    		
+			      });
 		  		} // end submitHandler
 
 			});
@@ -83,3 +83,92 @@
 	contactForm();
 
 })(jQuery);
+// new header js
+
+$(function() {
+
+  var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
+
+	var siteMenuClone = function() {
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();
+
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		})
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	};
+	siteMenuClone();
+
+});
